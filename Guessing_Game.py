114c805f33b3a5c0696a513d6cc501fa0cf3790f
Emka98 <115227079+Emka98@users.noreply.github.com>
2024-03-22 12:20:28 +0100

    Guessing_Game
    Find the correct number

---
+++ Guessing_Game.py
@@ -0,0 +1,30 @@
+from random import randint
+
+# Start the program with basic setup
+lower_num, upper_num = 1, 10
+random_number: int = randint(lower_num, upper_num)
+print(f'Guess the number in the range from {lower_num} to {upper_num}.')
+
+# Run an infinite loop for the game
+while True:
+    # Get the user guess
+    try:
+        user_guess: int = int(input('Guess: '))
+    except ValueError as e:
+        print('Please enter a valid number.')
+        continue
+
+    # Check the user guess
+    if user_guess > random_number:
+        if user_guess > upper_num:
+            print("Number out of range")
+        else:
+            print('The number is lower')
+    elif user_guess < random_number:
+        if user_guess < lower_num:
+            print("Number out of range")
+        else:
+            print('The number is higher')
+    else:
+        print('You guessed it!')
+        break
