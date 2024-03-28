5d839ff8637320f1731412d10e7c731a23099868
Emka98 <115227079+Emka98@users.noreply.github.com>
2024-03-28 19:53:58 +0100

    Automatic report search and copying

---
+++ LOTAMS - Automatic report search and copying.ps1
@@ -0,0 +1,13 @@
+﻿﻿$Source = 'C:\Users\60220895\Desktop\Segregator'
+$Destination = 'C:\Users\60220895\Desktop\SPR'
+
+if(-not(Test-Path -Path $Destination)){
+New-Item -Path $Destination -ItemType Directory
+}
+
+$lista = Get-ChildItem  $Source -recurse -include *ETD*,*TTWN*,*TTWO*,*CV*,*DR* | Select-Object DirectoryName
+
+foreach($file in $lista){
+Write-Host $file.DirectoryName
+Copy-Item $file.DirectoryName -Destination $Destination -Recurse
+}
